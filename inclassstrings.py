e498a2b318e1f180e231207b28d2f996b2f074c2
Joel Rejive <joelrejive2@gmail.com>
2026-09-01 16:28:49 +0000

    Completed code for string and variables and submitted on canvas

---
+++ inclassstrings.py
@@ -0,0 +1,5 @@
+x,y,z = 1,2,3
+print(f"x is {x} and y is {y} and z is {z}")
+
+name = input("What is your name? ")
+print(f"Your name is {name}")
---
+++ inclassvariables.py
@@ -0,0 +1,2 @@
+x = int(11 / 2)
+print(f"The number is {x ** 4}")
