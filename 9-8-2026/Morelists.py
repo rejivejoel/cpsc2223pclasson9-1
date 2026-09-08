cf0768977f36d26aa9398bb81412a2a35970f5e9
Joel Rejive <joelrejive2@gmail.com>
2026-09-08 16:39:45 +0000

    Finished all lecture notes

--- 9-8-2026/Morelists.py
+++ 9-8-2026/Morelists.py
@@ -2,4 +2,14 @@ num_list = list(range(0, 100))
 
 print(num_list)
 
-print(num_list(2:4))
+print(num_list(4))
+print(num_list(2))
+print(num_list(9))
+new_list = num_list[:]
+for n in new_list:
+    n *= 2
+
+print(f"The original list is: ")
+print(num_list)
+print(f"The new list is: ")
+print(new_list)
