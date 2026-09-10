b0aa0438e9b19e0e3e89da8894f7810baca8d79a
Joel Rejive <joelrejive2@gmail.com>
2026-09-10 15:15:51 +0000

    Finished first assignment

--- 9-10-2026/1.py
+++ 9-10-2026/1.py
@@ -1,4 +1,19 @@
 # Name: Joel Rejive
 # Student ID: 883464604
 # Section: CPSC 223P-07
-# Assignment: Module # Assignment #
+# Assignment: Module 2 Assignment 1
+
+g_list = []
+
+game1 = input("What is your favorite game? ")
+g_list.append(game1)
+
+game2 = input("What is your second favorite game? ")
+g_list.append(game2)
+
+game3 = input("What is your third favorite game? ")
+g_list.append(game3)
+
+print(f"One of your favorite games is {g_list.pop().title()}")
+print(f"One of your favorite games is {g_list.pop().title()}")
+print(f"One of your favorite games is {g_list.pop().title()}")
